63d9dc139e6bfa60ef5da765eb5575c0ddcd0bfd
Tony W <bobacrypto2@gmail.com>
2023-07-03 05:34:43 -0700

    Tried implementing 3D panoramic image, but deicded it would make the project beyond complex for the original nature of project

--- src/App.js
+++ src/App.js
@@ -1,31 +1,12 @@
 import React from "react";
-// import { createClient } from "@supabase/supabase-js";
-
-// const supabaseURL = process.env.REACT_APP_SUPABASE_URL;
-// const supabaseKey = process.env.REACT_APP_SUPABASE_KEY;
-// const supabase = createClient(supabaseURL, supabaseKey);
+import "./styles/app.css";
 
 function App() {
-  return <div></div>;
+  return (
+    <div className="main-container">
+      <div className="image-container"></div>
+    </div>
+  );
 }
 
 export default App;
-
-// const [countries, setCountries] = useState([]);
-
-// useEffect(() => {
-//   getCountries();
-// }, []);
-
-// async function getCountries() {
-//   const { data } = await supabase.from("countries").select();
-//   setCountries(data);
-// }
-
-// return (
-//   <ul>
-//     {countries.map((country) => (
-//       <li key={country.name}>{country.name}</li>
-//     ))}
-//   </ul>
-// );
