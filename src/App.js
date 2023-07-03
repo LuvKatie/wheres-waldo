c8ae4eee2312841e268462b7b660b4b76c9a4b18
Tony W <bobacrypto2@gmail.com>
2023-07-03 03:15:07 -0700

    Create all necessary files and our 360 image

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from "react";
+// import { createClient } from "@supabase/supabase-js";
+
+// const supabaseURL = process.env.REACT_APP_SUPABASE_URL;
+// const supabaseKey = process.env.REACT_APP_SUPABASE_KEY;
+// const supabase = createClient(supabaseURL, supabaseKey);
+
+function App() {
+  return <div></div>;
+}
+
+export default App;
+
+// const [countries, setCountries] = useState([]);
+
+// useEffect(() => {
+//   getCountries();
+// }, []);
+
+// async function getCountries() {
+//   const { data } = await supabase.from("countries").select();
+//   setCountries(data);
+// }
+
+// return (
+//   <ul>
+//     {countries.map((country) => (
+//       <li key={country.name}>{country.name}</li>
+//     ))}
+//   </ul>
+// );
