29a972f0c30e81cefab84a1fde417fe804a5bc13
Tony W <bobacrypto2@gmail.com>
2023-07-04 01:35:22 -0700

    Add image cover and start game button to hide image until user presses Start button

--- src/components/WaldoImage.js
+++ src/components/WaldoImage.js
@@ -6,6 +6,11 @@ const WaldoImage = () => {
   return (
     <>
       <img aria-label="pokemon-waldo" src={pokemonWaldo} id="waldo-image"></img>
+      <div data-testid="image-cover" className="image-cover cover">
+        <button aria-label="start-game" id="start-game">
+          Start
+        </button>
+      </div>
     </>
   );
 };
