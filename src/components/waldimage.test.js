506b9543c1770e7ccb4cfff1696411bda75af0b1
Tony W <bobacrypto2@gmail.com>
2023-07-04 01:12:37 -0700

    Write first tests for WaldoImage component for Start Game button and an image covering element

--- src/components/waldimage.test.js
+++ src/components/waldimage.test.js
@@ -1,12 +1,42 @@
-// import React from "react";
 import "@testing-library/jest-dom";
-// import { act } from "react-test-renderer";
-// import { render, screen } from "@testing-library/react";
-// import userEvent from "@testing-library/user-event";
-// import { MemoryRouter } from "react-router-dom";
-// import App from "./App";
+import { act } from "react-test-renderer";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { MemoryRouter } from "react-router-dom";
+import App from "./App";
 
 describe("WaldoImage component functionality", () => {
+  it("Image container contains a start game button", () => {
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    );
+    const startGame = screen.getByRole("button", { name: "start-game" });
+
+    expect(startGame).toBeInTheDocument();
+  });
+
+  it("Waldo image cover is displayed until Start Game button gets clicked", async () => {
+    const user = userEvent.setup();
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    );
+    const imageCover = screen.getByTestId("image-cover");
+
+    expect(imageCover).toHaveClass("cover");
+
+    const startGame = screen.getByRole("button", { name: "start-game" });
+    await act(async () => {
+      await user.click(startGame);
+    });
+
+    expect(imageCover).not.toHaveClass("cover");
+    expect(imageCover).toHaveClass("uncover");
+  });
+
   it.skip("When right clicking image show sprite options", () => {});
 
   it.skip("When clicking on option return callback function", () => {});
