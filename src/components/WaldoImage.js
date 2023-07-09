9e2dc67694bbd593aed0d75ddad74706b43e3f95
Tony W <bobacrypto2@gmail.com>
2023-07-09 04:44:31 -0700

    Fix unused import in WaldoImage.js

--- src/components/WaldoImage.js
+++ src/components/WaldoImage.js
@@ -1,4 +1,4 @@
-import React, { useRef, useContext, useEffect } from "react";
+import React, { useRef, useContext } from "react";
 import "../styles/waldoimage.css";
 import pokemonWaldo from "../images/pokemon.jpg";
 import { AppContext } from "../ContextProvider";
